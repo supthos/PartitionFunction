479d3266f8dbc83a47a650ce1691d832838898ad
Guillermo Manuel Dávila Andino <supermythos@outlook.com>
2025-01-11 02:30:22 -0500

    First Push. Add Source Code.

---
+++ PartitionFunction/IntPart.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+*   IntPart.cpp                                                               *
+*******************************************************************************
+*   2025 © Guillermo M. Davila Andino                                         *
+*   All rights reserved.                                                      *
+*******************************************************************************                                                                             *
+*   This program uses the "partition" function in "IntPart.h"                 *
+*   to compute any integer partition given enough resources.                  *
+******************************************************************************/
+
+#include "IntPart.h"
+
+int main() {
+	std::cout << "Integer Partition! ";
+	unsigned long long PartitionCount = 0;
+	while (true) {
+
+		unsigned long long N;
+
+		std::cout << "Enter Integer: ";
+		std::cin >> N;
+		std::cout << "Partitions:" << std::endl;
+		PartitionCount = 0;
+		for (unsigned long long P = 0; P <= N; P++) {
+			// Here, we exploit the fact that the integer partition of an integer equals
+			// the union of the partitions of that integer into K parts, for 1<=k<=N.
+			std::set<std::vector<unsigned long long>> PartitionResults = partition(N, P);
+			PartitionCount += PartitionResults.size();
+			for (auto& i : PartitionResults) {
+				PrntPrts(i);
+			}
+
+		}
+		std::cout << "\nTotal: " << std::to_string(PartitionCount) << std::endl;
+
+	}
+	return 0;
+}
---
+++ PartitionFunction/IntPart.h
@@ -0,0 +1,125 @@
+/******************************************************************************
+*   IntPart.h                                                                 *
+*******************************************************************************
+*   2025 © Guillermo M. Davila Andino                                         *
+*   All rights reserved.                                                      *
+*******************************************************************************
+*   This header file implements an integer partition function "partition"     *
+*   to part unsigned integers into k parts in standard ISO C++20.             *
+******************************************************************************/
+
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+void PrntPrts(std::vector<unsigned long long> Parts) {
+	std::string Partition;
+	for (unsigned long long j = 0; j < Parts.size(); j++) {
+		Partition = Partition + std::to_string(Parts[j]);
+		if (j >= 0 && (j + 1 < Parts.size())) Partition = Partition + ", ";
+	}
+	std::cout << Partition << std::endl;
+}
+
+std::set<std::vector<unsigned long long>> partition(unsigned long long Number, unsigned long long Parts) {
+	unsigned long long Sum = 0;
+	std::vector<unsigned long long> Par;
+
+	std::set<std::vector<unsigned long long>> PartitionTree;
+	std::set<std::vector<unsigned long long>> NewPartTree;
+	std::set<std::vector<unsigned long long>> Partitions;
+
+	if (Parts == 0) {
+		if (Number == 0) {
+			Par.push_back(0);
+			Partitions.insert(Par);
+		}
+		return Partitions;
+	}
+	for (unsigned long long i = 0; i < Parts; i++) {
+		Par.push_back(1);
+	}
+	std::sort(Par.begin(), Par.end());
+	while (Sum < Number) {
+		Sum = 0;
+		for (unsigned long long i : Par) {
+			Sum += i;
+
+		}
+		//auto j0 = Par;
+		//std::sort(Par.begin(), Par.end());
+
+		PartitionTree.insert(Par);
+
+		if (Par.size() > 0) Par[Parts - 1]++;
+	}
+	// Check for partitions
+	for (auto& i : PartitionTree) {
+		Sum = 0;
+		for (unsigned long long j : i) {
+			Sum += j;
+		}
+		if (Sum == Number) {
+			/*auto j0 = i;
+			std::sort(j0.begin(), j0.end());
+			Partitions.insert(j0);*/
+			Partitions.insert(i);
+		}
+	}
+
+
+	//for (unsigned long long i = 0; i < Number && !PartitionTree.empty(); i++) {
+	for (unsigned long long i = 0; i < Number; i++) {
+		NewPartTree.clear();
+		for (auto& j : PartitionTree) {
+
+			//for (unsigned long long j = 0; j < PartitionTree.size(); j++) {
+			Sum = 0;
+			for (unsigned long long k : j) {
+				Sum += k;
+			}
+			if (Sum <= Number && j[Parts - 1] >= i) {
+				Par = j;
+				std::sort(Par.begin(), Par.end());
+
+				for (unsigned long long k = 1; k < Number && Sum <= Number; k++) {
+
+					//auto j0 = Par;
+
+					NewPartTree.insert(Par);
+
+					Par[(Parts + Parts - k) % Parts]++;
+
+					Sum = 0;
+					for (unsigned long long l : Par) Sum += l;
+
+				}
+			}
+		}
+		PartitionTree.clear();
+		// Check for partitions
+		for (auto& j : NewPartTree) {
+			Sum = 0;
+			for (unsigned long long k : j) {
+				Sum += k;
+			}
+			if (Sum == Number) {
+				std::vector<unsigned long long> j0 = j;
+				std::sort(j0.begin(), j0.end());
+				Partitions.insert(j0);
+			}
+			if (j[Parts - 1] >= i + 1 && Sum < Number) {
+				std::vector<unsigned long long> j0 = j;
+				std::sort(j0.begin(), j0.end());
+
+				PartitionTree.insert(j0);
+
+			}
+		}
+	}
+
+	return Partitions;
+}
